400ff0d841b26f83bcc6631b152362c040a84a96
Renan Oliveira <renan.oliveira_10@hotmail.com>
2018-03-28 23:47:44 -0300

    retirando erros de compilacao

--- Program.cs
+++ Program.cs
@@ -17,10 +17,10 @@ namespace av2_sistemas_distribuidos
             Aviao aviao1 = new Aviao("Avião 1");
             Aviao aviao2 = new Aviao("Avião 2");
 
-            aviao1.Pousar(pista);
-            aviao2.Pousar(pista);
-            aviao1.Decolar();
-            aviao2.Pousar(pista);
+            // aviao1.Pousar(pista);
+            // aviao2.Pousar(pista);
+            // aviao1.Decolar();
+            // aviao2.Pousar(pista);
         }
 
         static void geraAviao(){
